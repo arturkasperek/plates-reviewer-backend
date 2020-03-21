78b2afd5c5086b885544a1f631892d934d2a08c0
Artur <arturmiks@gmail.com>
2020-03-21 10:25:15 +0100

    Adjusted deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ IMAGE=docker.pkg.github.com/arturkasperek/plates-reviewer-backend/plates-reviewe
 cat gh-token.txt | docker login docker.pkg.github.com -u arturkasperek --password-stdin
 docker pull $IMAGE
 docker stop plates-reviewer-backend-container || true && docker rm -f plates-reviewer-backend-container || true
-docker run --name=plates-reviewer-backend-container --restart=always -v /home/deploy-user/.env:/usr/src/app/.env -d $IMAGE
+docker run --name=plates-reviewer-backend-container -p 8080:8080 --restart=always -v /home/deploy-user/.env:/usr/src/app/.env -d $IMAGE
